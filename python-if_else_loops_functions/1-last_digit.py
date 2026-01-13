c5d2ebf00c666f77a1e3dcd1cf660e0df1d5f406
serkam31 <mmateomarques@gmail.com>
2026-01-13 17:48:56 +0100

    feat: implement last digit determination for random numbers

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+last = abs(number) % 10
+
+if number < 0:
+    last = -last
+
+if last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+elif last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5 and not 0")
+else:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
