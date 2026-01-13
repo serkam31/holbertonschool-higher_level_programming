d35ad32d2e4b8af0f8e09d4132056c51845da1c6
serkam31 <mmateomarques@gmail.com>
2026-01-13 17:50:43 +0100

    fix: simplify output message for last digit greater than 5

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ if number < 0:
 if last == 0:
     print(f"Last digit of {number} is {last} and is 0")
 elif last > 5:
-    print(f"Last digit of {number} is {last} and is greater than 5 and not 0")
+    print(f"Last digit of {number} is {last} and is greater than 5")
 else:
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
