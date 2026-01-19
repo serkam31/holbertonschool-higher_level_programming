64d8cbca86e28f20e49de3a89c94176e7766d68c
serkam31 <mmateomarques@gmail.com>
2026-01-19 15:09:41 +0100

    feat: add  a function that adds 2 tuples

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     new_string = ""
     for i in my_string:
         if i == 'c' or i == 'C':
-            new_string == " "
+            new_string == ""
         else:
             new_string += i
     return new_string
