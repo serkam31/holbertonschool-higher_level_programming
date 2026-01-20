fc0e5f1a1c3714f6532a47abc9f98965cd5151bd
serkam31 <mmateomarques@gmail.com>
2026-01-20 15:26:02 +0100

    feat: simplify best_score function to directly return the key with the highest value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    for key in a_dictionary:
-        best_key = max(a_dictionary)
-        return best_key
+    return max(a_dictionary, key=a_dictionary.get)
