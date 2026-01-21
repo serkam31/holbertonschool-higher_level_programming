568e562712b41af5348a131d799bbcd0f25d2328
serkam31 <mmateomarques@gmail.com>
2026-01-21 22:54:04 +0100

    fix: correct count increment position

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     while count < x:
         try:
             print("{}".format(my_list[count]), end="")
-            count += 1
         except IndexError:
             break
+        count += 1
     print()
     return count
