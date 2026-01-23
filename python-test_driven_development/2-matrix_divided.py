12f427287f5bbda1f907f4886ad351a127a61f75
serkam31 <mmateomarques@gmail.com>
2026-01-23 14:46:02 +0100

    feat: add matrix_divided function with full validation and rounding

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module that divides all elements of a matrix."""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix by div, rounded to 2 decimal places."""
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+    # Vérification de la structure initiale
+    if not isinstance(matrix, list) or not matrix or not isinstance(matrix[0], list):
+        raise TypeError(msg)
+
+    row_size = len(matrix[0])
+
+    # Vérification de div
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # Validation et calcul
+    new_matrix = []
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(msg)
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+
+        new_row = []
+        for i in row:
+            if not isinstance(i, (int, float)):
+                raise TypeError(msg)
+            # Calcul et arrondi directement dans l'append
+            new_row.append(round(i / div, 2))
+        new_matrix.append(new_row)
+
+    return new_matrix
