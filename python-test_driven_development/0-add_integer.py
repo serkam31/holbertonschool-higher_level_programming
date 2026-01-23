7d5919701c824fb3893a5a15ff88201c4cd1a470
serkam31 <mmateomarques@gmail.com>
2026-01-23 11:56:20 +0100

    feat: add float(inf) and float(nan) to the test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""Module that provides a function to add two integers."""
+
+
 def add_integer(a, b=98):
+    """Return the addition of a and b as integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a, b = int(a), int(b)
+
+    try:
+        a = int(a)
+    except (OverflowError, ValueError):
+        raise TypeError("a must be an integer")
+    try:
+        b = int(b)
+    except (OverflowError, ValueError):
+        raise TypeError("b must be an integer")
     return (a + b)
