2238f8de233fe5cac59aa3b5e093361bf8422913
serkam31 <mmateomarques@gmail.com>
2026-01-28 11:45:49 +0100

    feat: add Square class with size validation and area calculation

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with size validation"""
+
+
+class Square:
+    """Class that represents a square defined by its size"""
+    def __init__(self, size=0):
+        """Initialize a new square instance.
+        size: size of the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
