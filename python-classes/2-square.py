660f40d9a3ec19acb8f323138eab49e795380888
serkam31 <mmateomarques@gmail.com>
2026-01-28 10:24:48 +0100

    feat: implement  a class Square that defines a square

---
+++ python-classes/2-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 0-square defines an empty square class"""
+
+
+class Square:
+    """Empty class representing a square"""
+    def __init__(self, size):
+        self.__size = size
