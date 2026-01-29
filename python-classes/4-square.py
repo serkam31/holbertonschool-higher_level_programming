69442e644ed0f34b60f13a323ad378b0e3734d9f
serkam31 <mmateomarques@gmail.com>
2026-01-29 13:02:21 +0100

    feat: implement Square class with getter and setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,3 +19,16 @@ class Square:
         """Calculate and return the area of the square.
            Returns: the area of the square"""
         return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
