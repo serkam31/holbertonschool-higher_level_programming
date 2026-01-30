08cbed30d7798a7499adaba2ef1751d2d10ababf
serkam31 <mmateomarques@gmail.com>
2026-01-30 15:55:46 +0100

    feat: add static and class methods for rectangle comparison and square creation

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -72,6 +72,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
@@ -85,6 +86,7 @@ class Rectangle:
         else:
             return rect_2
 
+    @classmethod
     def square(cls, size=0):
         """Return a new Rectangle instance with width == height == size"""
         return cls(size, size)
