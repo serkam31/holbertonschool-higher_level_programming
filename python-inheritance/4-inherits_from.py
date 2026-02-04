f1c19206d4b294331c07344e172329c69e52c177
serkam31 <mmateomarques@gmail.com>
2026-02-04 10:02:38 +0100

    refactor: streamline inherits_from function by removing unnecessary conditional

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,5 +6,4 @@ from the specified class
 
 def inherits_from(obj, a_class):
     """Checks if obj is an instance of a class that inherited from a_class"""
-    if isinstance(obj, a_class) and type(obj) is not a_class:
-        return True
+    return isinstance(obj, a_class) and type(obj) is not a_class
