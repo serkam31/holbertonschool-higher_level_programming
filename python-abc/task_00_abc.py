6b0936298941106ed48b54a0135f3bc8aa5574d6
serkam31 <mmateomarques@gmail.com>
2026-02-04 11:52:28 +0100

    feat: create an abstract class that ensure that derived class implement sound method

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
-
+"""Task 00: Abstract Base Class Example"""
 from abc import ABC, abstractmethod
 
+
 class Animal(ABC):
+    """Abstract base class for animals."""
+    @abstractmethod
+    def sound(self):
+        """Method to be implemented by subclasses to return the sound of the animal."""
+        pass
+
+
+class Dog(Animal):
+    """Dog class implementing the Animal abstract base class."""
+    def sound(self):
+        """Return the sound made by the dog."""
+        return "Bark"
+
+
+class Cat(Animal):
+    """Cat class implementing the Animal abstract base class."""
+    def sound(self):
+        """Return the sound made by the cat."""
+        return "Meow"
