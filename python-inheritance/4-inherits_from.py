393d1abd474a99d9338859931ab02b0a68cfe6f4
serkam31 <mmateomarques@gmail.com>
2026-02-04 10:08:32 +0100

    refactor: enhance inherits_from function readability by restructuring return logic

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Function that checks if an object is an instance of a class that inherited
-from the specified class
+the specified class
 """
 
 
 def inherits_from(obj, a_class):
     """Checks if obj is an instance of a class that inherited from a_class"""
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
