83573cb405990cbe41088c833bfe05d2a034066e
serkam31 <mmateomarques@gmail.com>
2026-02-04 10:00:34 +0100

    refactor: simplify inherits_from function logic for clarity

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,5 @@ from the specified class
 
 def inherits_from(obj, a_class):
     """Checks if obj is an instance of a class that inherited from a_class"""
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
