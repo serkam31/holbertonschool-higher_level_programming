4bf6f1dbc36478bd7d190de568994afce4548de6
serkam31 <mmateomarques@gmail.com>
2026-03-04 16:25:35 +0100

    feat: add SQL scripts for inserting values and counting records in first_table

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Write a SQL statement to insert the following values in the first_table table:
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Write a query to count the number of records in the first_table where the id is 89.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
