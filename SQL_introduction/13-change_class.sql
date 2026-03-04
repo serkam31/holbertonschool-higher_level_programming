b4b77bc0d28462454e542c9a8a4ccd5d8a0d794c
serkam31 <mmateomarques@gmail.com>
2026-03-04 23:33:20 +0100

    fix: standardize SQL syntax for delete query in second_table

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
 -- Write a query to delete all records from second_table with a score less than or equal to 5.
-DELETE from second_table WHERE score <= 5;
+DELETE FROM second_table WHERE score <= 5;
