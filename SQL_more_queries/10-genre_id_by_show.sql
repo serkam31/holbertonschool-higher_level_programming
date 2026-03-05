8d094f975fd43d32722a8543a54dfb7297ff2465
serkam31 <mmateomarques@gmail.com>
2026-03-05 20:26:17 +0100

    feat: add SQL queries to list shows and their corresponding genre_ids in the database

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,6 @@
--- 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+-- Write a SQL query to list all the shows in the database hbtn_0d_tvshows and their corresponding genre_id. The tv_shows table contains the show id and title, and the tv_show_genres table contains the show id and genre_id. The results should be sorted by show title in ascending order and genre_id in ascending order.
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id 
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
