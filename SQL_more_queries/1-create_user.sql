55c313527a3f01a2a68412e240aaec32be860b49
serkam31 <mmateomarques@gmail.com>
2026-03-05 14:21:29 +0100

    fix: correct SQL syntax for user creation by adding host specification

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Write a SQL query to create a new user 'user_0d_1' with the password 'user_0d_1_pwd' and grant all privileges to this user.
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
